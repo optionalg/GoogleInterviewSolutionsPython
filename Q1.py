590d45539f6e825bab6ac5e3e92561dc5c51a2a1
Krishna Chaitanya Nelloore <kr1shna@outlook.com>
2017-05-30 02:04:02 -0400

    Corrected Q1 solution

--- Q1.py
+++ Q1.py
@@ -5,14 +5,14 @@ def solution(A, B):
     arr1 = []
     arrC =[]
     arr = []
-    minA = min(a)
-    minB = min(b)
     for i in range(len(a)):
-      arr.append(a.count(minA))
+      minA = min(a[i])
+      arr.append(a[i].count(minA))
     for i in range(len(b)):
-      arrC.append(b.count(minB))
+      minB = min(b[i])
+      arrC.append(b[i].count(minB))
     arr.sort()
-    arrC.sort()
-    for each in b:
-      arr1.append(sum(i > each for i in a))
+    arrC
+    for each in arrC:
+      arr1.append(sum(j < each for j in arr))
     return arr1
